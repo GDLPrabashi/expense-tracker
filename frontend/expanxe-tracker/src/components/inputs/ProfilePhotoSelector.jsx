0ab7b847311553e374d45e9423535b7603296cf4
Lakmini <lakminigarusinghe@gmail.com>
2025-05-12 00:15:31 +0530

    ✅ finished the signup/login UI

---
+++ frontend/expanxe-tracker/src/components/inputs/ProfilePhotoSelector.jsx
@@ -0,0 +1,66 @@
+import React, { useRef, useState } from 'react'
+import {LuUser,LuUpload,LuTrash} from "react-icons/lu"
+
+const ProfilePhotoSelector = ({image,setImage}) => {
+
+    const inputRef = useRef(null);
+    const [previewUrl, setPreviewUrl] = useState(null);
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if(file){
+            setImage(file);
+
+            const preview = URL.createObjectURL(file);
+            setPreviewUrl(preview);
+        }
+    }
+
+    const handleRemoveImage = () => {
+        setImage(null);
+        setPreviewUrl(null);
+    }
+
+    const onChoodeFile = () => {
+        inputRef.current.click();
+    }
+
+
+    return (
+        <div className='flex justify-center mb-6 '>
+            <input 
+            type='file'
+             className='hidden' 
+             ref={inputRef} 
+             onChange={handleImageChange} />
+
+
+            {!image ? (
+                <div className=' w-20 h-20 flex relative items-center justify-center gap-2 bg-purple-100 rounded-full cursor-pointer' onClick={onChoodeFile}>
+                  <LuUser size={40} className='text-primary'/>
+
+                  <button
+                  type='submit'
+                  className='w-8 h-8 flex items-center justify-center text-white absolute -bottom-1 -right-1 bg-primary rounded-full'
+                  onClick={onChoodeFile}
+                  >
+                    <LuUpload/>
+                  </button>
+        </div>
+            ) : (
+                <div className='flex flex-col items-center justify-center gap-2 cursor-pointer relative' onClick={onChoodeFile}>
+                  <img src={previewUrl} className='w-40 h-40 rounded-full object-cover  ' />
+                  <button
+                  type='submit'
+                  className='text-sm text-primary font-medium w-8 h-8 items-center justyfy-center bg-red-500 text-white absolute -bottom-1 -right-1 rounded-full -bottom-1 -right-1'
+                  onClick={handleRemoveImage}
+                  >
+                    <LuTrash/>
+                  </button>
+        </div>
+            )}
+        </div>
+  )
+}
+
+export default ProfilePhotoSelector
--- frontend/expanxe-tracker/src/pages/Auth/Signup.jsx
+++ frontend/expanxe-tracker/src/pages/Auth/Signup.jsx
@@ -1,8 +1,101 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import Authlayout from '../../components/layouts/Authlayout'
+import { useNavigate, Link } from 'react-router-dom'
+import Input from '../../components/inputs/input'
+import { validateEmail } from '../../utils/helper';
+import ProfilePhotoSelector from '../../components/inputs/ProfilePhotoSelector'
 
 const Signup = () => {
+
+  const [profilePic, setProfilePic] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const [error, setError] = useState('null');
+
+  const navigate = useNavigate();
+  
+  //handle signup form submit
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+  }
+
+  let profileImage ="";
+
+  if(!fullName || !email || !password) 
+    return setError('Please fill in all fields');
+  
+
+  if(profilePic) profileImage = URL.createObjectURL(profilePic);
+
+  setError('null');
+
+  //signup api call
+
+
+
   return (
-    <div>Signup</div>
+    <Authlayout>
+      <div className='lg:w-[100%] h-auto md:h-full mt-10 md:mt-0 flex flex-col justify-center '>
+        <h3 className='text-xl font-semibold text-black'>Create an Account</h3>
+        
+          <p className='text-sm text-slate-700 mt-[5px] mb-6'>
+          Join us today by entering your details below.
+        </p>
+
+        <form onSubmit={handleSignup}>
+
+        <ProfilePhotoSelector image={profilePic} setImage={setProfilePic} />
+
+          <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+          <Input 
+          value={fullName}
+          onChange={(e) => setFullName(e.target.value)}
+          label='Full Name'
+          placeholder='Enter Name'
+          />
+
+<Input
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            label="Email address"
+            placeholder='John@example.com'
+            type='text'
+          />
+
+          <div className='col-span-2'>
+          <Input
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            label="Password "
+            placeholder=''
+            type='password'
+          />
+          </div>
+          </div>
+
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
+                  
+                    <button type="submit" className='btn-primary'>
+                      SIGN UP
+                    </button>
+          
+                    <p className='text-[13px] text-slate-800 mt-3'>
+                      Already have an account ?{" "}
+                      <Link className="font-medium text-primary" to="/login">
+                      Login
+                      </Link>
+                    </p>
+                  </form>
+          
+          
+           
+          
+          
+      </div>
+    </Authlayout>
   )
 }
 
