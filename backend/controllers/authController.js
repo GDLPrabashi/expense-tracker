ee3636be74c905bd8cc4068d386085c2b14c1266
Lakmini <lakminigarusinghe@gmail.com>
2025-05-13 23:59:17 +0530

    feat: add user model, routes, and controllers

---
+++ backend/controllers/authController.js
@@ -0,0 +1,89 @@
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+//generate JWT token
+
+const generateToken = (id) => {
+    return jwt.sign({ id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+    });
+};
+
+
+//register user
+exports.registerUser = async (req, res) => {
+    const {fullName,email,password,profileImageUrl} = req.body;
+
+    //validatation:check for missing fields
+    if(!fullName ||!email || !password){
+        return res.status(400).json({message:"Please fill in all fields"});
+    }
+
+    try {
+        //check if email already exists
+
+        const existingUser = await User.findOne({email});
+        if(existingUser){
+            return  req.status(400).json({message:"User already exists"});
+        }
+
+        //create new user 
+        const user = await User.create({
+            fullName,
+            email,
+            password,
+            profileImageUrl:profileImageUrl || 'https://example.com/default-profile.png',
+        });
+
+        res.status(201).json({
+        id:user._id,
+        user,
+        token:generateToken(user._id),    
+    });
+} catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Error registering user",error:error.message});
+    }
+};
+
+//login user
+exports.loginUser = async (req, res) => {
+    const {email,password} = req.body;
+
+    //validation:check for missing fields
+    if(!email || !password){
+        return res.status(400).json({message:"Please fill in all fields"});
+    }
+
+    try{
+        const user = await User.findOne({email});
+
+        if(!user || !(await user.matchPassword(password))){
+            return res.status(401).json({message:"Invalid email or password"});
+        }
+
+        res.status(200).json({
+            id:user._id,
+            user,
+            token:generateToken(user._id),
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({message:"Error logging in user",error:error.message});
+    }
+};
+
+//get userinfro
+exports.getUserInfo = async(req,res) =>{
+    try {
+        const user = await findById(req.user.id).select("-password");
+        
+        if(!user){
+            return res.status(404).json({message:"User not found"});
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({message:"Error getting user info",error:error.message});
+    }
+};
--- backend/server.js
+++ backend/server.js
@@ -4,21 +4,29 @@ const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
+
 
 const app = express();
 
 
 
-app.use(cors(
+app.use(
     cors({
         origin: process.env.CLIENT_URL || "*",
         methods:['GET','POST','PUT','DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
     })
-));
+);
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Backend is running!');
+});
 connectDB();
+
+app.use("/api/v1/auth",authRoutes);
 
 const PORT = process.env.PORT || 5000;
 
