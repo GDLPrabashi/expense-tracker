c158442d92aa228d62f0d0cb3e0c81fc681c04de
Lakmini <lakminigarusinghe@gmail.com>
2025-05-17 21:31:02 +0530

    feat:add income model,routers and controllers

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
+const incomeRoutes = require("./routes/incomeRoutes");
 
 
 const app = express();
@@ -27,6 +28,7 @@ app.get('/', (req, res) => {
 connectDB();
 
 app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/income",incomeRoutes);
 
 //serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
